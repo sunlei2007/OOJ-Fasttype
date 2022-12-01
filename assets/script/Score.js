e9c1883a3e24108c6d8f6e4452c6146d09420d45
Lei Sun <139188566@qq.com>
2022-12-01 10:54:19 -0600

    Create index

---
+++ assets/script/Score.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+class Score {
+    #date
+    #hits
+    #percentage
+
+    constructor(date = new Date().toLocaleDateString(), hits=0, percentage=0) {
+        this.date = date;
+        this.hits = hits;
+        this.percentage = percentage;
+     }
+    set date(date) {
+        this.#date = date;
+     }
+    get date() {
+        return this.#date;
+     }
+    set hits(hits) {
+        this.#hits = hits;
+         
+     }
+    get hits() {
+        return this.#hits;
+     }
+    set percentage(percentage) {
+        this.#percentage = percentage;            
+     }
+    get percentage() {
+        return this.#percentage;
+     }
+ 
+}
+export {  Score }
---
+++ assets/script/index.js
@@ -0,0 +1,263 @@
+﻿'use strict';
+
+import { Score } from "./Score.js?v=1.1"
+
+const mainDiv = document.querySelector(".main-container");
+const arrWords = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 'population',
+    'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute',
+    'discipline', 'machine', 'accurate', 'connection', 'rainbow', 'bicycle',
+    'eclipse', 'calculator', 'trouble', 'watermelon', 'developer', 'philosophy',
+    'database', 'periodic', 'capitalism', 'abominable', 'component', 'future',
+    'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee', 'beauty', 'agency',
+    'chocolate', 'eleven', 'technology', 'alphabet', 'knowledge', 'magician',
+    'professor', 'triangle', 'earthquake', 'baseball', 'beyond', 'evolution',
+    'banana', 'perfumer', 'computer', 'management', 'discovery', 'ambition', 'music',
+    'eagle', 'crown', 'chess', 'laptop', 'bedroom', 'delivery', 'enemy', 'button',
+    'superman', 'library', 'unboxing', 'bookstore', 'language', 'homework',
+    'fantastic', 'economy', 'interview', 'awesome', 'challenge', 'science', 'mystery',
+    'famous', 'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
+    'keyboard', 'window'];
+
+const audioBg = new Audio("./assets/audio/bg.wav");
+const audioTrue = new Audio("./assets/audio/true.wav");
+const audioFalse = new Audio("./assets/audio/false.wav");
+const scoreObj = new Score();
+
+window.onload = function () {
+    const home = createHome();
+    mainDiv.appendChild(home);
+}
+function createHome() {
+    //Create home 
+    const divHome = document.createElement("div");
+    divHome.classList.add("home");
+    //Title
+    const pTitle = document.createElement("p");
+    pTitle.classList.add("home-title");
+    pTitle.innerHTML = "Fast Typer";
+    divHome.appendChild(pTitle);
+    //start button
+    const btnStart = document.createElement("button");
+    btnStart.classList.add("home-start");
+    btnStart.textContent = "Start";
+    divHome.appendChild(btnStart);
+    //button click event
+    btnStart.addEventListener("click", function () {
+        
+        audioBg.type = "audio/wav";
+        audioBg.loop = "loop";
+        audioBg.play();
+
+        pTitle.style.display = "none";
+        btnStart.style.display = "none";
+
+        const pCount = document.createElement("p");
+        pCount.classList.add("home-count");
+        divHome.appendChild(pCount);
+
+        //Add animation
+        let count = 4;
+        let timer=window.setInterval(() => {
+            count--;
+            if (count >= 0) {
+                pCount.classList.remove('home-count-active');
+                setTimeout(() => {   
+                    if (count === 0)
+                        pCount.innerHTML = "Go!";
+                    else
+                        pCount.innerHTML = count;
+
+                    pCount.classList.add('home-count-active');
+                }, 100);
+            } else {
+                
+                window.clearInterval(timer);
+                clearChild(mainDiv);//Remove all child;
+                const gameScene = createGameScene();
+                mainDiv.appendChild(gameScene);
+            }
+        }, 1000);
+    });
+
+    return divHome;
+}
+function createGameScene(){
+    const divGame = document.createElement("div");
+    divGame.classList.add("game");
+
+    //Create header
+    const divGameHeader = document.createElement("header");
+    divGameHeader.classList.add("game-header");
+    divGameHeader.classList.add("game-header-active");
+    divGame.appendChild(divGameHeader);
+    
+
+    //Create score
+    const pScore = document.createElement("p");
+    pScore.classList.add("game-score");
+    pScore.innerHTML = "Score: 0";
+    divGameHeader.appendChild(pScore);
+    //Create time
+    const pTime = document.createElement("p");
+    pTime.classList.add("game-time");
+    pTime.innerHTML = "Time: 40";
+    divGameHeader.appendChild(pTime);
+
+    //Create game content
+    const divGameContnet = document.createElement("div");
+    divGameContnet.classList.add("game-content");
+    
+
+    //Create word data
+    const pWord = document.createElement("p");
+    pWord.classList.add("game-word");
+    pWord.classList.add("game-word-active");
+    pWord.innerHTML = getRandomWord();
+    divGameContnet.appendChild(pWord);
+
+    //Create input
+    const inputValue = document.createElement("input");
+    inputValue.classList.add("game-input");
+    divGameContnet.appendChild(inputValue);
+
+    const checkMsg = document.createElement("i");
+    checkMsg.style.display = "none";
+    checkMsg.style.marginTop = "20px";
+    checkMsg.style.fontSize = "40px";
+    divGameContnet.appendChild(checkMsg);
+     
+
+    //Create footer
+    const divGameFooter = document.createElement("footer");
+    divGameFooter.classList.add("game-footer");
+    
+
+    setTimeout(() => {
+        divGameContnet.classList.add("game-content-active");
+        divGameFooter.classList.add("game-content-active");
+        divGame.appendChild(divGameContnet);
+        divGame.appendChild(divGameFooter);
+
+    }, 100);
+    
+
+    //restart button
+    const btnRetart = document.createElement("button");
+    btnRetart.classList.add("home-restart");
+    btnRetart.textContent = "Retart";
+    divGameFooter.appendChild(btnRetart);
+
+    //Control time
+    let count = 40;
+    let timer=window.setInterval(() => {
+        count--;
+        if (count >0) {
+            pTime.innerHTML = `Time: ${count}`;
+            if (count === 10) {
+                pTime.style.color = "red";
+            }
+        } else {
+            window.clearInterval(timer);
+
+            audioBg.pause();
+
+            scoreObj.date = new Date().toLocaleDateString();
+            scoreObj.hits = score;
+            scoreObj.percentage = (score / arrWords.length).toFixed(2) * 100;
+            
+            clearChild(mainDiv);//Remove all child;
+            const endScene = createEndScene();
+            mainDiv.appendChild(endScene);
+        }
+    }, 1000);
+    //Type word in input
+    let score = 0;
+    inputValue.addEventListener("input", function () {
+
+        checkMsg.style.display = "none";
+
+        if (pWord.innerHTML.substring(0, this.value.length).toLowerCase() !== this.value.toLowerCase()) {
+            checkMsg.style.display = "block";
+            checkMsg.style.color = "red";
+            checkMsg.className = "fa-regular fa-circle-xmark";
+
+            audioFalse.type = "audio/wav";
+            audioFalse.play();
+
+            inputValue.value = "";
+            pWord.innerHTML = getRandomWord();
+            pWord.style.color = getRandomColor();
+        }
+        else if (pWord.innerHTML.length === this.value.length) {
+            checkMsg.style.display = "block";
+            checkMsg.style.color = "green";
+            checkMsg.className = "fa-regular fa-circle-check";
+
+            audioTrue.type = "audio/wav";
+            audioTrue.play();
+
+            inputValue.value = "";
+            pWord.innerHTML = getRandomWord();
+            pWord.style.color = getRandomColor();
+            score++;
+            pScore.innerHTML = `Score: ${score}`;
+            
+
+        }
+    });
+    //Prohibit pasting
+    inputValue.onpaste = function(){
+        return false;
+    }
+    //Restart game
+    btnRetart.addEventListener("click", function () {
+        
+        count = 40;
+        pScore.innerHTML = `Score: 0`;
+        checkMsg.style.display = "none";
+        pWord.innerHTML = getRandomWord();
+        inputValue.value = "";
+    });
+    return divGame;
+}
+function createEndScene(){
+    const divHome = document.createElement("div");
+    divHome.classList.add("home");
+    //Score
+    const pTitle = document.createElement("p");
+    pTitle.classList.add("home-title");
+    pTitle.innerHTML = `Score: ${scoreObj.hits}`;
+    divHome.appendChild(pTitle);
+    //start button
+    const btnRetart = document.createElement("button");
+    btnRetart.classList.add("home-start");
+    btnRetart.textContent = "Retart";
+    divHome.appendChild(btnRetart);
+
+    btnRetart.addEventListener("click", function () {
+        audioBg.play();
+        clearChild(mainDiv);//Remove all child;
+        const gameScene = createGameScene();
+        mainDiv.appendChild(gameScene);
+    });
+
+    return divHome;
+}
+//Get random word
+function getRandomWord() {
+    
+    let index = Math.floor(Math.random() * (arrWords.length - 1) + 0);
+    return arrWords[index];
+}
+//Clear all child in div
+function clearChild(div) {
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+}
+function getRandomColor(){   
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
