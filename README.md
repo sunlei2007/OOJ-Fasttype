061288c23f77f08b08690d0caa610e4ba09831c0
Lei Sun <139188566@qq.com>
2022-12-08 12:42:45 -0600

    Add scoreboard

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# OOJ-Fasttype
-Click [here]( https://sunlei2007.github.io/OOJ-Fasttype/) to visit  
+## Typefighters
+<div align="center" ><img order-radius="100px" src="https://github.com/sunlei2007/OOJ-Fasttype/assets/img/game.png"/></div>
+<br>
+## How to play
+Complete the given typing within the specified time.
+<br>
+## Game link
+Click [here]( https://sunlei2007.github.io/OOJ-Fasttype/) to play  
--- assets/script/index.js
+++ assets/script/index.js
@@ -21,7 +21,7 @@ const arrWords = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'buildin
 const audioBg = new Audio("./assets/audio/bg.wav");
 const audioTrue = new Audio("./assets/audio/true.wav");
 const audioFalse = new Audio("./assets/audio/false.wav");
-const scoreObj = new Score();
+
 
 window.onload = function () {
     const home = createHome();
@@ -34,7 +34,7 @@ function createHome() {
     //Title
     const pTitle = document.createElement("p");
     pTitle.classList.add("home-title");
-    pTitle.innerHTML = "Fast Typer";
+    pTitle.innerHTML = "Type Fighter";
     divHome.appendChild(pTitle);
     //start button
     const btnStart = document.createElement("button");
@@ -43,7 +43,7 @@ function createHome() {
     divHome.appendChild(btnStart);
     //button click event
     btnStart.addEventListener("click", function () {
-        
+
         audioBg.type = "audio/wav";
         audioBg.loop = "loop";
         audioBg.play();
@@ -57,11 +57,11 @@ function createHome() {
 
         //Add animation
         let count = 4;
-        let timer=window.setInterval(() => {
+        let timer = window.setInterval(() => {
             count--;
             if (count >= 0) {
                 pCount.classList.remove('home-count-active');
-                setTimeout(() => {   
+                setTimeout(() => {
                     if (count === 0)
                         pCount.innerHTML = "Go!";
                     else
@@ -70,7 +70,7 @@ function createHome() {
                     pCount.classList.add('home-count-active');
                 }, 100);
             } else {
-                
+
                 window.clearInterval(timer);
                 clearChild(mainDiv);//Remove all child;
                 const gameScene = createGameScene();
@@ -81,7 +81,7 @@ function createHome() {
 
     return divHome;
 }
-function createGameScene(){
+function createGameScene() {
     const divGame = document.createElement("div");
     divGame.classList.add("game");
 
@@ -90,7 +90,7 @@ function createGameScene(){
     divGameHeader.classList.add("game-header");
     divGameHeader.classList.add("game-header-active");
     divGame.appendChild(divGameHeader);
-    
+
 
     //Create score
     const pScore = document.createElement("p");
@@ -106,7 +106,7 @@ function createGameScene(){
     //Create game content
     const divGameContnet = document.createElement("div");
     divGameContnet.classList.add("game-content");
-    
+
 
     //Create word data
     const pWord = document.createElement("p");
@@ -125,12 +125,12 @@ function createGameScene(){
     checkMsg.style.marginTop = "20px";
     checkMsg.style.fontSize = "40px";
     divGameContnet.appendChild(checkMsg);
-     
+
 
     //Create footer
     const divGameFooter = document.createElement("footer");
     divGameFooter.classList.add("game-footer");
-    
+
 
     setTimeout(() => {
         divGameContnet.classList.add("game-content-active");
@@ -139,7 +139,7 @@ function createGameScene(){
         divGame.appendChild(divGameFooter);
 
     }, 100);
-    
+
 
     //restart button
     const btnRetart = document.createElement("button");
@@ -149,27 +149,35 @@ function createGameScene(){
 
     //Control time
     let count = 40;
-    let timer=window.setInterval(() => {
+    let timer = window.setInterval(() => {
         count--;
-        if (count >0) {
+        if (count > 0) {
             pTime.innerHTML = `Time: ${count}`;
             if (count === 10) {
                 pTime.style.color = "red";
             }
         } else {
             window.clearInterval(timer);
-
             audioBg.pause();
 
-            scoreObj.date = new Date().toLocaleDateString();
-            scoreObj.hits = score;
-            scoreObj.percentage = (score / arrWords.length).toFixed(2) * 100;
-            
+            saveScore(getDate(), score, ((score / arrWords.length) * 100).toFixed(1));
+
             clearChild(mainDiv);//Remove all child;
             const endScene = createEndScene();
             mainDiv.appendChild(endScene);
         }
     }, 1000);
+
+    //ex Dec 6, 2022
+    function getDate() {
+        const options = {
+            year: "numeric",
+            month: "short",
+            day: "numeric"
+        }
+        //return new Date().toLocaleDateString("zh-cn", options);
+        return new Date().toLocaleDateString("en-ca", options);
+    }
     //Type word in input
     let score = 0;
     inputValue.addEventListener("input", function () {
@@ -201,17 +209,19 @@ function createGameScene(){
             pWord.style.color = getRandomColor();
             score++;
             pScore.innerHTML = `Score: ${score}`;
-            
+
 
         }
     });
     //Prohibit pasting
-    inputValue.onpaste = function(){
+    inputValue.onpaste = function () {
         return false;
     }
     //Restart game
     btnRetart.addEventListener("click", function () {
-        
+
+        saveScore(getDate(), score, ((score / arrWords.length) * 100).toFixed(1));
+        score = 0;
         count = 40;
         pTime.style.color = "#5fb7f8";
         pScore.innerHTML = `Score: 0`;
@@ -221,15 +231,45 @@ function createGameScene(){
     });
     return divGame;
 }
-function createEndScene(){
+function createEndScene() {
     const divHome = document.createElement("div");
-    divHome.classList.add("home");
-    //Score
+    divHome.classList.add("end");
+
+    //title
     const pTitle = document.createElement("p");
-    pTitle.classList.add("home-title");
-    pTitle.innerHTML = `Score: ${scoreObj.hits}`;
+    pTitle.classList.add("end-title");
+    pTitle.innerHTML = "HIGH SCORES";
     divHome.appendChild(pTitle);
-    //start button
+
+    //background
+    const divBackground = document.createElement("div");
+    divBackground.classList.add("end-div-background");
+    divHome.appendChild(divBackground);
+
+    //Score list
+    const table = document.createElement("table");
+    table.classList.add("end-table");
+    const arrScore = getScore().sort(compare("hits"));
+
+    for (let i = 0; i < arrScore.length; i++) {
+        const tr = document.createElement("tr");
+        const td = document.createElement("td");
+        table.appendChild(tr);
+        tr.appendChild(td);
+        td.innerHTML = `#${i + 1}`;
+
+        const td2 = document.createElement("td");
+        td2.innerHTML = `${arrScore[i].hits} words`;
+        tr.appendChild(td2);
+
+        const td3 = document.createElement("td");
+        td3.innerHTML = `${arrScore[i].percentage}%`;
+        tr.appendChild(td3);
+    }
+
+    divBackground.appendChild(table);
+
+    //Start button
     const btnRetart = document.createElement("button");
     btnRetart.classList.add("home-start");
     btnRetart.textContent = "Retart";
@@ -244,9 +284,28 @@ function createEndScene(){
 
     return divHome;
 }
+//Save score
+const arrScore = [];
+function saveScore(date, hits, percentage) {
+    const scoreObj = {};
+    scoreObj.date = date;
+    scoreObj.hits = hits;
+    scoreObj.percentage = percentage;
+    arrScore.push(scoreObj);
+
+    localStorage.setItem("scores", JSON.stringify(arrScore));
+}
+//Get score
+function getScore() {
+    let arr = [];
+    if (localStorage.getItem("scores")) {
+        arr = JSON.parse(localStorage.getItem("scores"));
+    }
+    return arr;
+}
 //Get random word
 function getRandomWord() {
-    
+
     let index = Math.floor(Math.random() * (arrWords.length - 1) + 0);
     return arrWords[index];
 }
@@ -256,9 +315,23 @@ function clearChild(div) {
         div.removeChild(div.firstChild);
     }
 }
-function getRandomColor(){   
+function getRandomColor() {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`;
+}
+//Array sort 
+function compare(property) {
+
+    return function (a, b) {
+
+        var value1 = a[property];
+
+        var value2 = b[property];
+
+        return value2 - value1;
+
+    }
+
 }
